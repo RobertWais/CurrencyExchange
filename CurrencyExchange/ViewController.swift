14f8c464d9a510a85be16a8ad324ff21236cdaf9
RobertWais <wais.robert@uwlax.edu>
2018-07-04 09:52:16 -0700

    creating UI

--- CurrencyExchange/ViewController.swift
+++ CurrencyExchange/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var buttonLayoutCollection: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        uiUpdate()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnPressed(_ sender: UIButton) {
+        var tag = sender.tag
+        print("The button tag is \(tag)")
+        if tag == 12 {
+            //Value C
+        }else{
+            //Number 1-9 or .
+        }
+    }
+    
+    func calculateExchange(){
+        //Take both strings, conver them
+    }
+    
+    func uiUpdate(){
+        for button in buttonLayoutCollection {
+            button.layer.borderWidth = 0.5
+            button.layer.borderColor = UIColor.lightGray.cgColor
+        }
+    }
+    
 
 }
 
